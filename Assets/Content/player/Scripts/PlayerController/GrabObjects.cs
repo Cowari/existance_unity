d19d6195a6858c40ff41eed52e7dc3b79a384540
Cowari <btccowar@gmail.com>
2025-01-27 02:59:34 +0300

    Обновлена система поднятия объектов + плавные вращение&&позиция

--- Assets/Content/player/Scripts/PlayerController/GrabObjects.cs
+++ Assets/Content/player/Scripts/PlayerController/GrabObjects.cs
@@ -1,14 +1,13 @@
-using System.Collections;
 using UnityEngine;
 
 public class GrabObjects : MonoBehaviour
 {
     public Transform grab_pos;
     public RayCast rayCast;
-    private RaycastHit hit;
     private GameObject grbd_obj;
     private Rigidbody rigbd_obj;
     private bool isGrab;
+    const float smoothSpeed = 4f;
 
     void Update(){
         TryGrab();
@@ -17,40 +16,37 @@ public class GrabObjects : MonoBehaviour
 
     void TryGrab(){
         if(rayCast.hit.collider!=null){
-            hit = rayCast.hit;
-        }
-        
-    
-        if(Input.GetAxis("Fire1")==1){
-            if(hit.collider.tag=="pickupable"){
-                grbd_obj = hit.collider.gameObject;
-                rigbd_obj = grbd_obj.GetComponent<Rigidbody>();
-                isGrab = true;
+            if(Input.GetButton("Fire1")){
+                if(rayCast.hit.collider.CompareTag("pickupable")){
+                    grbd_obj = rayCast.hit.collider.gameObject;
+                    rigbd_obj = grbd_obj.GetComponent<Rigidbody>();
+                    isGrab = true;
+                }
             }
-        }else{
-            isGrab = false;
         }
+        if(!Input.GetButton("Fire1"))isGrab = false;
 
         if(isGrab) Grab();
-        if(rigbd_obj!=null && !isGrab) Drop();
+        else if(rigbd_obj!=null) Drop();
     }
 
-    void Grab(){  
-        rigbd_obj.useGravity = false;
-        rigbd_obj.isKinematic = true;
+    void Grab(){
+        if(grbd_obj!=null && rigbd_obj!=null){
+            rigbd_obj.useGravity = false;
+            rigbd_obj.isKinematic = true;
 
-        grbd_obj.transform.position = grab_pos.position;
+            grbd_obj.transform.rotation = Quaternion.Lerp(grbd_obj.transform.rotation, grab_pos.rotation, smoothSpeed * Time.deltaTime);
+            grbd_obj.transform.position = Vector3.Lerp(grbd_obj.transform.position, grab_pos.position, smoothSpeed * Time.deltaTime);
+        }
     }
     
     void Drop(){
-        rigbd_obj.useGravity = true;
-        rigbd_obj.isKinematic = false; 
-    }
-
-    /*IEnumerator ObjPosition(){
-        while(true){
-
-            yield return new WaitForSeconds(0.1f);
+        if(grbd_obj!=null && rigbd_obj!=null){
+            rigbd_obj.useGravity = true;
+            rigbd_obj.isKinematic = false; 
+            
+            grbd_obj = null;
+            rigbd_obj = null;
         }
-    }*/
+    }
 }
